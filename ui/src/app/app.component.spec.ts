1e1e58a7705d3cc635cb852fbea17b7ac06b4f9a
mevlüt çifcibaşı <nebioo@users.noreply.github.com>
2020-06-18 22:24:35 +0300

    update angular project name

--- ui/src/app/app.component.spec.ts
+++ ui/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'clean'`, () => {
+  it(`should have as title 'Clean'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('clean');
+    expect(app.title).toEqual('Clean');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('clean app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Clean app is running!');
   });
 });
